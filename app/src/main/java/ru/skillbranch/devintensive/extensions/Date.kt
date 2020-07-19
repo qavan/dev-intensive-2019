dc58279e8e198b76dded8fb464fb3a8efe3afc40
qavan <pro100qavan@gmail.com>
2020-07-19 18:18:26 +0300

    yeah?

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -36,35 +36,35 @@ enum class TimeUnits {
     SECOND,
     MINUTE,
     HOUR,
-    DAY
-}
+    DAY;
 
-fun TimeUnits.plural(int: Int):String {
-    when (this) {
-        TimeUnits.SECOND ->
-            when (int % 10) {
-                1 -> return "$int секунду"
-                in 2..4 -> return "$int секунды"
-                0,in 5..9 -> return "$int секунд"
-            }
-        TimeUnits.MINUTE ->
-            when (int % 10) {
-                1 -> return "$int минуту"
-                in 2..4 -> return "$int минуты"
-                0,in 5..9 -> return "$int минут"
-            }
-        TimeUnits.HOUR ->
-            when (int % 10) {
-                1 -> return "$int час"
-                in 2..4 -> return "$int часа"
-                0,in 5..9 -> return "$int часов"
-            }
-        TimeUnits.DAY ->
-            when (int % 10) {
-                1 -> return "$int день"
-                in 2..4 -> return "$int дня"
-                0,in 5..9 -> return "$int дней"
+    fun plural(int:Int):String {
+        when (this) {
+            SECOND ->
+                when (int % 10) {
+                    1 -> return "$int секунду"
+                    in 2..4 -> return "$int секунды"
+                    0,in 5..9 -> return "$int секунд"
+                }
+            MINUTE ->
+                when (int % 10) {
+                    1 -> return "$int минуту"
+                    in 2..4 -> return "$int минуты"
+                    0,in 5..9 -> return "$int минут"
+                }
+            HOUR ->
+                when (int % 10) {
+                    1 -> return "$int час"
+                    in 2..4 -> return "$int часа"
+                    0,in 5..9 -> return "$int часов"
+                }
+            DAY ->
+                when (int % 10) {
+                    1 -> return "$int день"
+                    in 2..4 -> return "$int дня"
+                    0,in 5..9 -> return "$int дней"
+                }
         }
+        return ""
     }
-    return ""
 }
